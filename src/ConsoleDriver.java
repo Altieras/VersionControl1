5ef1fd94d28b62065d2f02fb8648b220036ce097
Altieras <RossBM19@gcc.edu>
2022-11-09 13:37:25 -0500

    Console Driver with notes

---
+++ src/ConsoleDriver.java
@@ -0,0 +1,17 @@
+public class ConsoleDriver {
+    public static void main(String[] args) {
+        //bulk of logic is elsewhere
+        //this should only have I/O logic
+        //meaning talk to user, get user input, show changes to user
+
+        //eg
+        //create an object of type Whatever
+        //Whatever w = new Whatever();
+        //maybe while(!w.hasWon())
+        //ask user for input
+        //call w.displayStatus() ------- or displayBoard()
+
+        //can add a gui with minimal if any changes to game logic
+
+    }
+}

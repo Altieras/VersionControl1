ab2aea99e9f1a84f2137ac0b2995c451e2647941
HagenSV <HagenSV22@gcc.edu>
2022-11-10 14:02:44 -0500

    Song stub class

---
+++ src/Song.java
@@ -0,0 +1,44 @@
+import java.io.File;
+
+public class Song {
+    
+    /** Loads a file to play audio from
+     * @throws FileFormatException if the file is an improper audio format
+     */
+    public Song(File f){
+        
+    }
+
+    /** Plays the file at the designated starting time 
+     * @param time the time (in seconds) at which to start playing the file at
+    */
+    public void play(int time){
+        
+    }
+
+    /** Stops playing the file */
+    public void pause(){
+        
+    }
+
+    /** Checks if the audio file is currently being played
+     * @return true if the file is being played
+     */
+    public boolean playing(){
+        return true;
+    }
+
+    /**
+     * @return the namev of the song
+     */
+    public String getName(){
+        return null;
+    }
+
+    /**
+     * @return the length of the song in seconds
+     */
+    public int getDuration(){
+        return 0;
+    }
+}

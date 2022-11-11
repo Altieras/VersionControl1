6beba7a877191a4df14177be9210828798acc685
HagenSV <HagenSV22@gcc.edu>
2022-11-11 13:18:38 -0500

    Miscellaneous AudioPlayer changes

--- src/AudioPlayer.java
+++ src/AudioPlayer.java
@@ -1,19 +1,21 @@
 import java.util.LinkedList;
-import java.util.Queue;
 
 /** This class will be used as the primary interface for interacting with the program */
-public abstract class AudioPlayer {
+public class AudioPlayer {
     /** List of songs to be played after the current song */
-    private Queue<Song> queue;
+    private LinkedList<Song> queue;
+    
     /** The song that is currently playing */
     private Song currentSong;
 
+    /** */
     private int savedTime;
+    
     /** List of songs that have been previously played */
     private LinkedList<Song> previousSongs;
 
     /** The maximum amount of songs allowed in the queue and previous songs */
-    private final int MAX_LENGTH = 50;
+    private static final int MAX_LENGTH = 50;
 
 
     /** Plays the current song starting at the saved timestamp */
@@ -49,6 +51,20 @@ public abstract class AudioPlayer {
     /** Adds every song in a playlist to the queue */
     public void addToQueue(Playlist p, boolean shuffle){
 
+    }
+
+    /**
+     * @return true if there is a song waiting in the queue
+     */
+    public boolean hasNext(){
+        return false;
+    }
+
+    /**
+     * @return true if there is a song saved
+     */
+    public boolean hasPrevious(){
+        return false;
     }
 
     /** Returns a list of all the songs currently in the queue */
